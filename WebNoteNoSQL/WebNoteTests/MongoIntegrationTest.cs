147ce894fcb8053596b649353ee1b83c7f3e879a
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2013-02-25 15:28:31 +0100

    preparing a MSpec style test to show the setup of Mongo2Go, added yet another unit test framework

---
+++ WebNoteNoSQL/WebNoteTests/MongoIntegrationTest.cs
@@ -0,0 +1,20 @@
+﻿using Mongo2Go;
+using MongoDB.Driver;
+
+namespace WebNoteTests
+{
+    public class MongoIntegrationTest
+    {
+        internal static MongoDbRunner _runner;
+        internal static MongoCollection<TestDocument> _collection;
+
+        internal static void CreateConnection()
+        {
+            _runner = MongoDbRunner.Start();
+
+            MongoServer server = new MongoClient(_runner.ConnectionString).GetServer();
+            MongoDatabase database = server.GetDatabase("IntegrationTest");
+            _collection = database.GetCollection<TestDocument>("TestCollection");
+        }
+    }
+}
