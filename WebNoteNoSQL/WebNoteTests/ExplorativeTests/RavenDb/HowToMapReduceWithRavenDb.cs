30c6f6ad2050829e8c0c98e4564f0441802b6af9
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2013-06-25 02:26:04 +0200

    simplified ravendb map/reduce index example

--- WebNoteNoSQL/WebNoteTests/ExplorativeTests/RavenDb/HowToMapReduceWithRavenDb.cs
+++ WebNoteNoSQL/WebNoteTests/ExplorativeTests/RavenDb/HowToMapReduceWithRavenDb.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.ComponentModel.Composition.Hosting;
 using System.Linq;
 using NUnit.Framework;
 using Raven.Abstractions.Data;
 using Raven.Client;
 using Raven.Client.Document;
 using Raven.Client.Extensions;
+using Raven.Client.Indexes;
 using WebNoteNoSQL.Code.DbStartScript;
 using WebNoteNoSQL.Models;
 using WebNoteTests.ExplorativeTests.Infrastructure;
@@ -31,25 +33,49 @@ namespace WebNoteTests.ExplorativeTests.RavenDb
         }
 
         [Test]
-        [Ignore]
         public void DoMapReduce()
         {
-            new SearchIndex().Execute(store);
+            //// A cleaner way --> AbstractIndexCreationTask<TDocument,TReduceResult>
+            // var assembly = typeof(Logs_SearchIndex).Assembly;
+            // var catalog = new CompositionContainer(new AssemblyCatalog(assembly));
+            // IndexCreation.CreateIndexes(catalog, store.DatabaseCommands.ForDatabase(Database), store.Conventions);
+
+            store.DatabaseCommands.ForDatabase(Database).PutIndex(
+                "Logs/SearchIndex",
+                new IndexDefinitionBuilder<LogEntry, Logs_SearchIndex.Result>
+                {
+                    Map =  logEntries => from entry in logEntries
+                                         select new Logs_SearchIndex.Result
+                                         { 
+                                             Country = entry.Country,
+                                             Size = entry.Size,
+                                             Count = 1
+                                         },
+
+                    Reduce = results => from entry in results
+                                        group entry by entry.Country into e
+                                        select new Logs_SearchIndex.Result
+                                        {
+                                            Country = e.Key,
+                                            Count = e.Sum(x => x.Count),
+                                            Size = e.Sum(x => x.Size),
+                                        }
+                }, true);
+
 
             using (var session = store.OpenSession(Database))
             {
-                var reducedResults = session.Query<SearchIndex.Result, SearchIndex>()
-                    .ToList();
-                
+                var reducedResults = session.Query<Logs_SearchIndex.Result, Logs_SearchIndex>().ToList();
+
+                Console.WriteLine("{0,25} {1,10} {2,15}", "Country", "Count", "Size");
                 foreach (var result in reducedResults)
                 {
-                    Console.WriteLine("Country: " + result.Country + "\tCount: " + result.Count + "\tSize:" + result.Size);
+                    Console.WriteLine("{0,25} {1,10} {2,15}", result.Country, result.Count, result.Size);
                 }
             }
         }
 
         [Test]
-        [Ignore]
         public void EnsureDatabaseHasLogEntries()
         {
             using (var session = store.OpenSession(Database))
